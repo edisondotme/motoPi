23f09934e5ae0baa8f92cca6c6c69078cdf72a01
Edison Orellana <me@edison.me>
2017-02-12 21:11:17 +0000

    got a working time page

--- display/views.py
+++ display/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import time
 
 
 def index(request):
-	# Note, in the future, this should display [X].html
-	# depending on what sort of display i want to render
-	# hm on second thought, no there should just be another
-	# function definition shouldn't there?
 	# I guess the index here should ask what graph you want
 	# to display.
 	return render(request, 'display/display_home.html')
+
+
+def disp_time(request):
+	currTime = time.strftime("%H:%M:%S", time.gmtime())
+
+	data = {'time': currTime}
+	return render(request, 'display/time.html', data)
