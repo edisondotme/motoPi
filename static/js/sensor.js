00a3bd0f763f8a85a75624449618d6faf0c840f8
Edison Orellana <me@edison.me>
2017-02-26 22:47:49 +0000

    git is hard

---
+++ static/js/sensor.js
@@ -0,0 +1,20 @@
+
+$(function() {
+		// When we're using HTTPS, use WSS too.
+		var ws_scheme = window.location.protocol == "https:" ? "wss" : "ws";
+		var chatsock = new ReconnectingWebSocket(ws_scheme + '://' + window.location.host + "/sensors/light/");
+
+		chatsock.onopen = function() {
+					 console.log("Connected!");
+					 $('#sensor').text("Connected!");
+					 chatsock.send("Connected!");
+		};
+
+		chatsock.onmessage = function(message) {
+				console.log("Received Sock message!");
+				console.log(message);
+				$('#sensor').append(message.data);
+				$('#second-sensor').append(message.newData);
+		};
+
+});
